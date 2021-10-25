4d1245504dbe7fda810770d50c984055df9dcefd
Vitaly Yulis <vitaly.yulis@gmail.com>
2021-10-25 09:57:29 +0300

    fix interface linking to sys-deps

--- cmake/bastard.cmake
+++ cmake/bastard.cmake
@@ -1,6 +1,6 @@
 include_guard(GLOBAL)
 
-set(BASTARD_VERSION "0.16.0")
+set(BASTARD_VERSION "0.16.1")
 cmake_policy(VERSION 3.10)
 
 # TODO: fix the problem of unloading sys-dependencies (for example Thread) without specifying the language
